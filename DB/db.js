8b923dd33f52661fd0847ac9beeefd42f63f4be1
dev <devanshsahu333@gamil.com>
2024-06-25 20:45:21 +0530

    fixed db timeout

--- DB/db.js
+++ DB/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const mongoDBUrl = process.env.MONGODB_URL;
 
-mongoose.connect(mongoDBUrl)
+mongoose.connect(mongoDBUrl,{
+    serverSelectionTimeoutMS: 50000, // Increase timeout to 50 seconds
+    socketTimeoutMS: 45000, // Increase socket timeout to 45 seconds    
+})
     .then(() => {
         console.log('Successfully connected to MongoDB');
     })
