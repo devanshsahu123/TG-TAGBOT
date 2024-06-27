c83e689ad3b40fb1f892a1265f81d3dbac3bdbc7
dev <devanshsahu333@gamil.com>
2024-06-28 00:34:04 +0530

    deploy

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const groupJobUpdater = require('./cron/cron-group-job.js');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json());
+app.use(
+    express.json( )
+);
 
 (async () => {
     await groupJobUpdater();
