2aa5f1c48f037dd81ed0342fda271e2de7f906af
dev <devanshsahu333@gamil.com>
2024-07-04 23:17:02 +0530

    time away

--- bot-Functions/afkHandler.js
+++ bot-Functions/afkHandler.js
@@ -57,14 +57,17 @@ function timeAway(startDate) {
     const diffInHours = Math.floor(diffInMinutes / 60);
     const diffInDays = Math.floor(diffInHours / 24);
     const diffInMonths = Math.floor(diffInDays / 30);
+    const diffInYears = Math.floor(diffInMonths / 12);
 
-    if (diffInMonths > 0) {
+    if (diffInYears > 0) {
+        return `${diffInYears} year${diffInYears > 1 ? 's' : ''}`;
+    } else if (diffInMonths > 0) {
         return `${diffInMonths} month${diffInMonths > 1 ? 's' : ''}`;
     } else if (diffInDays > 0) {
         return `${diffInDays} day${diffInDays > 1 ? 's' : ''}`;
     } else if (diffInHours > 0) {
         return `${diffInHours} hour${diffInHours > 1 ? 's' : ''}`;
-    } else if (diffInMinutes>0){
+    } else if (diffInMinutes > 0) {
         return `${diffInMinutes} minute${diffInMinutes > 1 ? 's' : ''}`;
     } else {
         return `${diffInSeconds} second${diffInSeconds > 1 ? 's' : ''}`;
